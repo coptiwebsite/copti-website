af5ad531c7fa50b52add2f300c1e294332c5f371
coptiwebsite <copti.website@gmail.com>
2026-04-26 22:57:35 +0000

    Add files via upload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
-
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import Script from 'next/script';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta name="google-site-verification" content="gGJ9Zm7NUWdtF-H-M283_8GAHnPSNQuh3rrsoOdQy44" />
+      </Head>
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-QDGD481DEB"
         strategy="afterInteractive"
