cabaaca2b14cb156b1331af81a9a6c7aa87d1dd1
coptiwebsite <copti.website@gmail.com>
2026-04-26 17:55:45 +0000

    Add files via upload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
+
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-QDGD481DEB"
+        strategy="afterInteractive"
+      />
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-QDGD481DEB');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
